9265938db0702d2af60ba83cefeffa3ae52367b7
Glen Rutherford <grutherford@ofigroup.com.au>
2021-05-28 15:37:14 +1000

    Added script to reduce the output json file down to the useful elements

---
+++ src/process-output.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+
+const fileDirectory = 'output'
+const inputFileName = 'extractPdfInput.pdf.json'
+
+async function readFile(filePath) {
+  try {
+    const buffer = await fs.readFile(filePath);
+    
+    const data = JSON.parse(buffer.toString())
+
+    const reducedData = data.elements.map(element=> 
+        {return {
+            path: element.Path,
+            text: element.Text,
+            objectId: element.ObjectID,
+            page: element.Page
+        }})
+
+        console.log(JSON.stringify(reducedData, null, 2))
+    
+    return reducedData
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error.message}`);
+  }
+}
+
+async function writeFile(fileDirectory, inputFileName, text) {
+    try {
+      await fs.writeFile(`${fileDirectory}/reduced_${inputFileName}`, text, { flag: 'w' });
+    } catch (error) {
+      console.error(`Got an error trying to write to a file: ${error.message}`);
+    }
+  }
+  
+  (async function () {
+    const reducedData = await readFile(`${fileDirectory}/${inputFileName}`);
+    await writeFile(fileDirectory, inputFileName, JSON.stringify(reducedData, null, 2));
+  })();
+
